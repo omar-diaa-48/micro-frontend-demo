436b404bce9653236d98140368dfa4c9027e4f78
omar <o.wakeel@intouch.com>
2023-11-19 10:11:43 +0200

    feat: configure container app

--- container/vite.config.ts
+++ container/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import federation from "@originjs/vite-plugin-federation";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import packageJson from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    port: 3000
+  },
+  plugins: [
+    react(),
+    federation({
+      name: 'container',
+      remotes: {
+        'LandingApp': 'http://localhost:3001/remoteEntry.js'
+      },
+      shared: [packageJson.dependencies]
+    })
+  ],
 })
