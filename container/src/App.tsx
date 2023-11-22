fcb9218b9d34b582fcb213c48454703f9535ffb1
omar <o.wakeel@intouch.com>
2023-11-22 07:54:07 +0200

    feat: use router to switch between sub apps

--- container/src/App.tsx
+++ container/src/App.tsx
@@ -1,15 +1,27 @@
-import { lazy } from 'react'
+import { lazy, Suspense } from 'react'
+import {
+  Route,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
 import './App.css'
 
 const LazyVueLanding = lazy(() => import("./components/LandingVue"))
 const LazyReactLanding = lazy(() => import("./components/LandingReact"))
 
 function App() {
+  const navigate = useNavigate();
   return (
     <div className="container" >
       <p>This is from container react</p>
-      <LazyVueLanding />
-      <LazyReactLanding />
+      <button onClick={() => navigate('/vue')}>Show vue remote</button>
+      <button onClick={() => navigate('/react')}>Show react remote</button>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/vue' Component={LazyVueLanding} />
+          <Route path='/react' Component={LazyReactLanding} />
+        </Routes>
+      </Suspense>
     </div>
   )
 }
--- container/src/main.tsx
+++ container/src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('_container-dev-root')!).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
 )
